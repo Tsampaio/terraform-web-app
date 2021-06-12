2e5a7805c54236d60340a76946d4996ede3a1d31
Telmo Sampaio <telmoasampaio@gmail.com>
2021-06-12 18:36:20 +0100

    create public subnets

--- vpc_networking/main.tf
+++ vpc_networking/main.tf
@@ -3,10 +3,30 @@ provider "aws" {
 }
 
 resource "aws_vpc" "module_vpc" {
-  cidr_block = var.vpc_cidr_block
+  cidr_block           = var.vpc_cidr_block
   enable_dns_hostnames = true
 
   tags {
-     Name="Production-VPC"
+    Name = "Production-VPC"
+  }
+}
+
+resource "aws_subnet" "module_public_subnet_1" {
+  cidr_block        = var.public_subnet_1_cidr
+  vpc_id            = aws_vpc.module_vpc.id
+  availability_zone = "${var.region}a"
+
+  tags {
+    Name = "Public-Subnet-1"
+  }
+}
+
+resource "aws_subnet" "module_public_subnet_2" {
+  cidr_block        = var.public_subnet_2_cidr
+  vpc_id            = aws_vpc.module_vpc.id
+  availability_zone = "${var.region}b"
+
+  tags {
+    Name = "Public-Subnet-2"
   }
 }
