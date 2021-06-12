e841633bcdfa5bc505ba769e07c027a5cae61701
Telmo Sampaio <telmoasampaio@gmail.com>
2021-06-12 23:15:15 +0100

    Implementing EC2 Instance

--- vpc_networking/main.tf
+++ vpc_networking/main.tf
@@ -122,3 +122,47 @@ resource "aws_route" "igw_route" {
   gateway_id = aws_internet_gateway.internet_gateway.id
   destination_cidr_block = "0.0.0.0/0"
 }
+
+data "aws_ami" "ubuntu_latest" {
+  owners = ["099720109477"]
+  most_recent = true
+
+  filter {
+    name = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+# aws ec2 create-key-pair --key-name my-first-ec2-instance --region eu-west-2 --query 'KeyMaterial' --output text > my-first-ec2-instance.pem
+# resource "aws_key_pair" "" {
+#   public_key = ""
+# }
+
+resource "aws_instance" "my-first-ec2-instance" {
+  ami = data.aws_ami.ubuntu_latest.id
+  instance_type = "t2.micro"
+  key_name = "my-first-ec2-instance"
+  security_groups = [aws_security_group.ec2-security-group.id]
+  subnet_id = aws_subnet.module_public_subnet_1.id
+
+  # user_data = "" our custom script to run when the instance is created
+}
+
+resource "aws_security_group" "ec2-security-group" {
+  name = "EC2-Instance-SG"
+  vpc_id = aws.module_vpc.id
+
+  ingress {
+    from_port = 0
+    protocol = "-1" //all traffic from aws
+    to_port = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    protocol = "-1" //all traffic from aws
+    to_port = 0
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
